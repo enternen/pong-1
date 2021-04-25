b8c19f7421f9eebca3f4d068ecce8d02e80c8540
Kirill Grenader <okirich@users.noreply.github.com>
2021-04-25 11:24:35 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -16,33 +16,34 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 520:
+        if keys[K_s] and self.rect.y < 455:
             self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 520:
+        if keys[K_DOWN] and self.rect.y < 455:
             self.rect.y += self.speed
-
+# параметры окна
 window = display.set_mode((600,500))
 window.fill((200,255,255))
-
+# игровой таймер
 game = True
 clock = time.Clock()
 
 speed_x = 3
 speed_y = 3
 # создаем спрайты для игры
-# racket1 = Player("",30,200,4,50,150)
-# racket2 = Player("",520,200,4,50,150)
-# ball = Player("",200,200,4,50,50)
+racket1 = Player("racket.png",30,200,4,50,15)
+racket2 = Player("racket.png",520,200,4,50,15)
+ball = Player("ball.png",200,200,25,25,50)
 # создаем надписи
 font.init()
 font = font.Font(None,35)
 lose1 = font.render("PLAYER 1 LOSE!",True,(180,0,0))
 lose2 = font.render("PLAYER 2 LOSE!",True,(180,0,0))
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -53,7 +54,27 @@ while game:
         racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
-        ball.rect.y += speed_y 
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > 500-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
+ 
+        if ball.rect.x > 600:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
 
     display.update()
     clock.tick(60)
